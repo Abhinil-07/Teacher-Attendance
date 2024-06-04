724314c1cad4d325b8f0beaa9abf0d1a08ffe5e1
Abhinil-07 <abhinilnath10@gmail.com>
2024-06-04 23:17:45 +0530

    Navbar updated

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -28,9 +28,9 @@ const Navbar: React.FC = () => {
       <div className="flex items-center gap-4">
         <ModeToggle />
         <div className="flex items-center gap-2">
-          {/* <Avatar>
+          <Avatar>
             <AvatarImage alt="Avatar" src="/placeholder-user.jpg" />
-          </Avatar> */}
+          </Avatar>
           <div className="text-sm font-medium">{user.username}</div>
         </div>
         <Button className="rounded-full" size="icon" variant="ghost">
